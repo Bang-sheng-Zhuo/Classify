aa243e8af547aa784e3de3314e360a97885a1270
Ben <416084473@qq.com>
2018-10-20 10:23:27 +0800

    update

--- Main_VGG16.py
+++ Main_VGG16.py
@@ -10,7 +10,7 @@ from tensorflow.contrib import learn
 from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
 
 tf.logging.set_verbosity(tf.logging.INFO)
-
+a = 1
 
 # CIFAR-100-dataLoad
 class cifarDataLoader():
